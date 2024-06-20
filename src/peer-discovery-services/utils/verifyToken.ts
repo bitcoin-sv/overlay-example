84cf7352368b5dad0d031116996780cb9f2c632f
Brayden Langley <brayden167@gmail.com>
2024-06-20 10:16:39 -0700

    VerifyToken cannot validate expectedPublicKey, only sig

--- src/peer-discovery-services/utils/verifyToken.ts
+++ src/peer-discovery-services/utils/verifyToken.ts
@@ -1,5 +1,4 @@
 import { PublicKey, Signature } from '@bsv/sdk'
-import { getPaymentAddress } from 'sendover'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -16,20 +15,8 @@ export const verifyToken = (
   identityKey: string,
   lockingPublicKey: string,
   fields: Buffer[],
-  signature: string,
-  protocolId: string
+  signature: string
 ): void => {
-  const expectedPublicKey = getPaymentAddress({
-    senderPrivateKey: process.env.SERVER_PRIVATE_KEY,
-    recipientPublicKey: identityKey,
-    invoiceNumber: `2-${protocolId}-1`, // BRC-43 formatted invoice number
-    returnType: 'publicKey'
-  })
-
-  if (expectedPublicKey !== lockingPublicKey) {
-    throw new Error('Invalid locking key!')
-  }
-
   const pubKey = PublicKey.fromString(lockingPublicKey)
   const hasValidSignature = pubKey.verify(
     Array.from(Buffer.concat(fields)),
